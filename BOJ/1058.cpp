06cfb2ece0876e6fee5dd04506fea32aab650c80
jimin <parkjm012@sogang.ac.kr>
2025-09-08 20:47:27 +0900

    1058-깔끔ver

--- BOJ/1058.cpp
+++ BOJ/1058.cpp
@@ -1,52 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
 using namespace std;
 
-bool isFriend(vector<vector<int>> peo, int i, int j){
-    if(peo[i][j]) return true;
-    else {
-        for(int k=0;k<(int)peo[i].size();k++){
-            if(peo[i][k]){
-                if(peo[k][j]) return true;
-            }
-        }
-    }
-    return false;
-}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     int n;
-    cin>>n;
+    cin >> n;
+
     vector<vector<int>> people(n, vector<int>(n));
     string line;
-    for(int i=0;i<n;i++){
-        cin>>line;
-        for(int j=0;j<n;j++){
-            if(line[j]=='N'){
-                people[i][j]=0;
-            }
-            else{
-                people[i][j]=1;
-            }
+
+    for (int i = 0; i < n; i++) {
+        cin >> line;
+        for (int j = 0; j < n; j++) {
+            people[i][j] = (line[j] == 'Y');
         }
     }
-    vector<int> count(n);
-    int max=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i!=j && isFriend(people, i, j)) {
-                count[i]++;
+
+    int maxCount = 0;
+    for (int i = 0; i < n; i++) {
+        vector<bool> visited(n, false);  // 친구/2-친구 표시
+        for (int j = 0; j < n; j++) {
+            if (people[i][j]) {          // 직접 친구
+                visited[j] = true;
+                for (int k = 0; k < n; k++) {
+                    if (people[j][k]) {  // 친구의 친구
+                        visited[k] = true;
+                    }
+                }
             }
         }
-        if(count[i]>max) max=count[i];
+        visited[i] = false;  // 자기 자신 제외
+
+        int cnt = 0;
+        for (bool v : visited) if (v) cnt++;
+        maxCount = max(maxCount, cnt);
     }
 
-    cout<<max;
-
-    return 0;
-
+    cout << maxCount;
 }
