ec0acf43a5392c29c5e54e39683e5673f66043ac
jimin <parkjm012@sogang.ac.kr>
2025-09-25 11:10:12 +0900

    week03

--- ProblemSolvingPractice/week03/1.cpp
+++ ProblemSolvingPractice/week03/1.cpp
@@ -1,116 +1,203 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// void solve_row_wise() {
+//     // 입출력 속도 향상
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int N, Q;
+//     cin >> N >> Q;
+
+//     // 1. 초기 행렬 A 입력
+//     // (인덱스 편의를 위해 1-based로 N+1 크기 할당)
+//     vector<vector<int>> A(N + 1, vector<int>(N + 1));
+//     for (int i = 1; i <= N; ++i) {
+//         for (int j = 1; j <= N; ++j) {
+//             cin >> A[i][j];
+//         }
+//     }
+
+//     // 보조 행렬 D 생성 (변화량 기록용)
+//     // C2+1 접근을 위해 열 크기를 N+2로 할당
+//     vector<vector<int>> D(N + 1, vector<int>(N + 2, 0));
+
+//     // 2. 쿼리 처리
+//     for (int k = 0; k < Q; ++k) {
+//         int r1, r2, c1, c2, v;
+//         cin >> r1 >> r2 >> c1 >> c2 >> v;
+
+//         // 쿼리가 영향을 미치는 모든 행(r1 ~ r2)을 순회
+//         for (int i = r1; i <= r2; ++i) {
+//             // 각 행에 1차원 구간 갱신 규칙 적용
+//             D[i][c1] += v;
+//             D[i][c2 + 1] -= v;
+//         }
+//     }
+
+//     // 3. 누적 합 계산 및 최종 결과 출력
+//     for (int i = 1; i <= N; ++i) {
+//         for (int j = 1; j <= N; ++j) {
+//             // 3-1. 각 행의 누적 합 계산
+//             D[i][j] += D[i][j - 1];
+            
+//             // 3-2. 초기 행렬에 변화량 적용
+//             A[i][j] += D[i][j];
+            
+//             cout << A[i][j] << " ";
+//         }
+//         cout << "\n";
+//     }
+// }
+// // 이벤트를 저장하기 위한 구조체
+// struct Event {
+//     int val;
+//     int c1;
+//     int c2;
+// };
+
+// void solve_scanline() {
+//     // 입출력 속도 향상
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int N, Q;
+//     cin >> N >> Q;
+
+//     // 1. 초기 행렬 A 입력
+//     vector<vector<int>> A(N + 1, vector<int>(N + 1));
+//     for (int i = 1; i <= N; ++i) {
+//         for (int j = 1; j <= N; ++j) {
+//             cin >> A[i][j];
+//         }
+//     }
+
+//     // 1. 이벤트 생성
+//     // 각 행에 발생할 이벤트를 저장할 리스트
+//     // (r2+1 접근을 위해 행 크기를 N+2로 할당)
+//     vector<vector<Event>> events(N + 2);
+//     for (int k = 0; k < Q; ++k) {
+//         int r1, r2, c1, c2, v;
+//         cin >> r1 >> r2 >> c1 >> c2 >> v;
+//         events[r1].push_back({v, c1, c2});      // r1 행에 시작 이벤트 추가
+//         events[r2 + 1].push_back({-v, c1, c2}); // r2+1 행에 종료 이벤트 추가
+//     }
+
+//     // 2. 스캔 및 계산
+//     // 현재 행의 누적 변화량을 기록할 1차원 상태 배열
+//     vector<int> D_row(N + 2, 0);
+//     for (int i = 1; i <= N; ++i) { // 1행부터 N행까지 스캔
+//         // 2-1. 현재 행(i)에서 발생하는 모든 이벤트 처리
+//         if (!events[i].empty()) {
+//             for (const auto& event : events[i]) {
+//                 D_row[event.c1] += event.val;
+//                 D_row[event.c2 + 1] -= event.val;
+//             }
+//         }
+
+//         // 2-2. 현재 행의 최종 값 계산 및 출력
+//         int current_change = 0;
+//         for (int j = 1; j <= N; ++j) {
+//             current_change += D_row[j];
+//             A[i][j] += current_change;
+//             cout << A[i][j] << " ";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+
+// int main() {
+//     solve_row_wise();
+//     solve_scanline();
+//     return 0;
+// }
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 using namespace std;
 
-void solve_row_wise() {
-    // 입출력 속도 향상
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+struct query {
+    int R1, R2, C1, C2;
+    double V;
+};
 
-    int N, Q;
-    cin >> N >> Q;
-
-    // 1. 초기 행렬 A 입력
-    // (인덱스 편의를 위해 1-based로 N+1 크기 할당)
-    vector<vector<int>> A(N + 1, vector<int>(N + 1));
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) {
-            cin >> A[i][j];
-        }
-    }
-
-    // 보조 행렬 D 생성 (변화량 기록용)
-    // C2+1 접근을 위해 열 크기를 N+2로 할당
-    vector<vector<int>> D(N + 1, vector<int>(N + 2, 0));
-
-    // 2. 쿼리 처리
-    for (int k = 0; k < Q; ++k) {
-        int r1, r2, c1, c2, v;
-        cin >> r1 >> r2 >> c1 >> c2 >> v;
-
-        // 쿼리가 영향을 미치는 모든 행(r1 ~ r2)을 순회
-        for (int i = r1; i <= r2; ++i) {
-            // 각 행에 1차원 구간 갱신 규칙 적용
-            D[i][c1] += v;
-            D[i][c2 + 1] -= v;
-        }
-    }
-
-    // 3. 누적 합 계산 및 최종 결과 출력
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) {
-            // 3-1. 각 행의 누적 합 계산
-            D[i][j] += D[i][j - 1];
-            
-            // 3-2. 초기 행렬에 변화량 적용
-            A[i][j] += D[i][j];
-            
-            cout << A[i][j] << " ";
-        }
-        cout << "\n";
-    }
-}
-// 이벤트를 저장하기 위한 구조체
-struct Event {
-    int val;
+struct event{
+    double v;
     int c1;
     int c2;
 };
 
-void solve_scanline() {
-    // 입출력 속도 향상
+void matrix_addition(int N, vector<vector<double>>& A, int Q, vector<query>& queries) {
+    //TODO:
+    vector<vector<event>> events(N+2);
+
+    for(int i=0;i<Q;i++){
+        int r1=queries[i].R1;
+        int r2=queries[i].R2;
+        int c1=queries[i].C1;
+        int c2=queries[i].C2;
+        double v = queries[i].V;
+        events[r1].push_back({v,c1,c2});
+        events[r2+1].push_back({-v,c1,c2});
+    }
+    vector<double> Hap (N+2,0);
+
+    for(int i=1;i<N+1;i++){
+        double currentHap=0;
+        while(!events[i].empty()){
+            event tmp= events[i].back();
+            events[i].pop_back();
+            Hap[tmp.c1]+=tmp.v;
+            Hap[tmp.c2+1]-=tmp.v;
+        }
+
+        for(int j=1;j<N+1;j++){
+            currentHap+=Hap[j];
+            A[i-1][j-1]+=currentHap;
+        }
+
+
+    }
+
+}
+
+int main() {
+    // Do NOT delete these lines unless you know what you are doing:
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N, Q;
-    cin >> N >> Q;
+    int N;
+    cin >> N;
 
-    // 1. 초기 행렬 A 입력
-    vector<vector<int>> A(N + 1, vector<int>(N + 1));
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) {
+    vector<vector<double>> A(N, vector<double>(N));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cin >> A[i][j];
         }
     }
 
-    // 1. 이벤트 생성
-    // 각 행에 발생할 이벤트를 저장할 리스트
-    // (r2+1 접근을 위해 행 크기를 N+2로 할당)
-    vector<vector<Event>> events(N + 2);
-    for (int k = 0; k < Q; ++k) {
-        int r1, r2, c1, c2, v;
-        cin >> r1 >> r2 >> c1 >> c2 >> v;
-        events[r1].push_back({v, c1, c2});      // r1 행에 시작 이벤트 추가
-        events[r2 + 1].push_back({-v, c1, c2}); // r2+1 행에 종료 이벤트 추가
+    int Q;
+    cin >> Q;
+
+    vector<query> queries(Q);
+    for (int i = 0; i < Q; i++) {
+        cin >> queries[i].R1 >> queries[i].C1 >> queries[i].R2 >> queries[i].C2 >> queries[i].V;
     }
 
-    // 2. 스캔 및 계산
-    // 현재 행의 누적 변화량을 기록할 1차원 상태 배열
-    vector<int> D_row(N + 2, 0);
-    for (int i = 1; i <= N; ++i) { // 1행부터 N행까지 스캔
-        // 2-1. 현재 행(i)에서 발생하는 모든 이벤트 처리
-        if (!events[i].empty()) {
-            for (const auto& event : events[i]) {
-                D_row[event.c1] += event.val;
-                D_row[event.c2 + 1] -= event.val;
-            }
-        }
+    matrix_addition(N, A, Q, queries);
 
-        // 2-2. 현재 행의 최종 값 계산 및 출력
-        int current_change = 0;
-        for (int j = 1; j <= N; ++j) {
-            current_change += D_row[j];
-            A[i][j] += current_change;
-            cout << A[i][j] << " ";
+    cout << fixed << setprecision(12);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << A[i][j] << ' ';
         }
-        cout << "\n";
+        cout << '\n';
     }
-}
 
-
-int main() {
-    solve_row_wise();
-    solve_scanline();
     return 0;
 }
--- ProblemSolvingPractice/week03/2.cpp
+++ ProblemSolvingPractice/week03/2.cpp
@@ -1,109 +1,279 @@
-#include <iostream>
-#include <vector>
-#include <numeric>
-#include <algorithm>
-#include <map>
+// #include <iostream>
+// #include <vector>
+// #include <numeric>
+// #include <algorithm>
+// #include <map>
 
-// 각 턴의 카드 세트를 저장하기 위한 구조체
-struct TurnCards {
-    long long cardA, cardB, cardC;
-};
+// // 각 턴의 카드 세트를 저장하기 위한 구조체
+// struct TurnCards {
+//     long long cardA, cardB, cardC;
+// };
 
-// isBobWin DP 테이블에서 특정 상태의 값을 안전하게 조회하는 함수
-// 키가 존재하지 않으면 false를 반환 (Bob의 필승 상태가 아님)
-bool checkWinState(const std::map<long long, bool>& layer, long long sum) {
-    auto it = layer.find(sum);
-    if (it != layer.end()) {
-        return it->second;
+// // isBobWin DP 테이블에서 특정 상태의 값을 안전하게 조회하는 함수
+// // 키가 존재하지 않으면 false를 반환 (Bob의 필승 상태가 아님)
+// bool checkWinState(const std::map<long long, bool>& layer, long long sum) {
+//     auto it = layer.find(sum);
+//     if (it != layer.end()) {
+//         return it->second;
+//     }
+//     return false;
+// }
+
+// int main() {
+//     // C++ 입출력 속도 향상
+//     std::ios_base::sync_with_stdio(false);
+//     std::cin.tie(nullptr);
+
+//     int N; // 총 턴 수
+//     long long L, R; // 목표 점수 구간
+//     std::cin >> N >> L >> R;
+
+//     std::vector<TurnCards> turns(N);
+//     for (int i = 0; i < N; ++i) {
+//         std::cin >> turns[i].cardA >> turns[i].cardB >> turns[i].cardC;
+//     }
+
+//     // --- 1단계: '밥의 필승 지도' 완성 ---
+//     std::vector<long long> futureMinGain(N + 1, 0);
+//     std::vector<long long> futureMaxGain(N + 1, 0);
+
+//     for (int i = N - 1; i >= 0; --i) {
+//         futureMinGain[i] = futureMinGain[i + 1] + turns[i].cardA;
+//         futureMaxGain[i] = futureMaxGain[i + 1] + turns[i].cardC;
+//     }
+
+//     std::vector<std::map<long long, bool>> isBobWin(N + 2);
+
+//     for (long long s = L; s <= R; ++s) {
+//         isBobWin[N + 1][s] = true;
+//     }
+
+//     for (int i = N; i >= 1; --i) {
+//         long long lower_bound = L - futureMaxGain[i];
+//         long long upper_bound = R - futureMinGain[i];
+
+//         for (long long s = lower_bound; s <= upper_bound; ++s) {
+//             int winnableFutures = 0;
+//             const auto& nextLayer = isBobWin[i + 1];
+//             if (checkWinState(nextLayer, s + turns[i - 1].cardA)) winnableFutures++;
+//             if (checkWinState(nextLayer, s + turns[i - 1].cardB)) winnableFutures++;
+//             if (checkWinState(nextLayer, s + turns[i - 1].cardC)) winnableFutures++;
+//             if (winnableFutures >= 2) {
+//                 isBobWin[i][s] = true;
+//             }
+//         }
+//     }
+
+//     // --- 최종 판정 및 전략 탐색 ---
+//     if (checkWinState(isBobWin[1], 0)) {
+//         // 1턴, 합 0이 Bob의 필승 상태이므로 Bob이 이김
+//         std::cout << "Bob" << std::endl;
+//     } else {
+//         // Alice가 이기는 경우, 전략을 출력
+//         std::cout << "Alice" << std::endl;
+        
+//         long long currentSum = 0;
+//         // --- 2단계: 앨리스의 최적 경로 탐색 ---
+//         for (int i = 1; i <= N; ++i) {
+//             const auto& nextLayer = isBobWin[i + 1];
+//             const auto& currentTurnCards = turns[i - 1];
+
+//             // 시나리오 1: Alice가 A를 금지할 경우
+//             // Bob에게 남는 B, C가 모두 Bob의 패배로 이어지는지 확인
+//             bool banA_is_safe = !checkWinState(nextLayer, currentSum + currentTurnCards.cardB) &&
+//                                 !checkWinState(nextLayer, currentSum + currentTurnCards.cardC);
+//             if (banA_is_safe) {
+//                 std::cout << "Ban " << currentTurnCards.cardA << std::endl;
+//                 // Bob은 남은 카드 중 작은 것을 선택한다고 가정하여 합계 업데이트
+//                 currentSum += currentTurnCards.cardB;
+//                 continue;
+//             }
+
+//             // 시나리오 2: Alice가 B를 금지할 경우
+//             bool banB_is_safe = !checkWinState(nextLayer, currentSum + currentTurnCards.cardA) &&
+//                                 !checkWinState(nextLayer, currentSum + currentTurnCards.cardC);
+//             if (banB_is_safe) {
+//                 std::cout << "Ban " << currentTurnCards.cardB << std::endl;
+//                 currentSum += currentTurnCards.cardA;
+//                 continue;
+//             }
+
+//             // 시나리오 3: Alice가 C를 금지할 경우
+//             // (Alice가 이기는 게임에서는 위 세 시나리오 중 하나는 반드시 참임)
+//             std::cout << "Ban " << currentTurnCards.cardC << std::endl;
+//             currentSum += currentTurnCards.cardA;
+//         }
+//     }
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+long long N, L, R, arr[40005][3];
+
+// 4개의 함수의 "함수 원형(인자/반환형/이름)"은 절대 바꾸면 안됩니다.
+// 전역 변수/함수/클래스 정의 등은 자유롭게 수정하거나 추가해도 됩니다.
+// 정답을 최종 제출할 때 I/O(입출력)는 진행하면 안됩니다. 채점기는 이 4개의 함수만 호출하며 I/O는 엄격히 금지합니다.
+// 디버그, 테스트 목적의 출력은 허용됩니다. (e.g. printf, cout)
+// I/O를 조작하여 부당한 점수를 얻은 경우, 향후 재채점에서 0점 처리됩니다.
+
+//DP 테이블 i 턴 시작 시 Bob이 이낄 수 있는 합계의 구간들 저장
+vector<vector<pair<long long, long long>>> BobWinIntervals;
+
+vector<pair<long long, long long>> intersect(const vector<pair<long long, long long>>& A, const vector<pair<long long, long long>>& B){
+    vector<pair<long long, long long>> result;
+    int i=0, j=0;
+    while(i<A.size() && j <B.size()){
+        long long start = max(A[i].first, B[j].first);
+        long long end = min(A[i].second,B[j].second);
+
+        if(start<=end){
+            result.push_back({start,end});
+        }
+
+        if(A[i].second < B[j].second){
+            i++;
+        }else{
+            j++;
+        }
     }
-    return false;
+    return result;
 }
 
-int main() {
-    // C++ 입출력 속도 향상
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    int N; // 총 턴 수
-    long long L, R; // 목표 점수 구간
-    std::cin >> N >> L >> R;
-
-    std::vector<TurnCards> turns(N);
-    for (int i = 0; i < N; ++i) {
-        std::cin >> turns[i].cardA >> turns[i].cardB >> turns[i].cardC;
-    }
-
-    // --- 1단계: '밥의 필승 지도' 완성 ---
-    std::vector<long long> futureMinGain(N + 1, 0);
-    std::vector<long long> futureMaxGain(N + 1, 0);
-
-    for (int i = N - 1; i >= 0; --i) {
-        futureMinGain[i] = futureMinGain[i + 1] + turns[i].cardA;
-        futureMaxGain[i] = futureMaxGain[i + 1] + turns[i].cardC;
-    }
-
-    std::vector<std::map<long long, bool>> isBobWin(N + 2);
-
-    for (long long s = L; s <= R; ++s) {
-        isBobWin[N + 1][s] = true;
-    }
-
-    for (int i = N; i >= 1; --i) {
-        long long lower_bound = L - futureMaxGain[i];
-        long long upper_bound = R - futureMinGain[i];
-
-        for (long long s = lower_bound; s <= upper_bound; ++s) {
-            int winnableFutures = 0;
-            const auto& nextLayer = isBobWin[i + 1];
-            if (checkWinState(nextLayer, s + turns[i - 1].cardA)) winnableFutures++;
-            if (checkWinState(nextLayer, s + turns[i - 1].cardB)) winnableFutures++;
-            if (checkWinState(nextLayer, s + turns[i - 1].cardC)) winnableFutures++;
-            if (winnableFutures >= 2) {
-                isBobWin[i][s] = true;
-            }
+vector<pair<long long, long long>> union_i ( const vector<pair<long long, long long>>& A, const vector<pair<long long, long long>>& B){
+    vector<pair<long long, long long>> merged;
+    int i=0, j=0;
+    while(i<A.size() || j<B.size()){
+        if(i<A.size() && (j== B.size() || A[i]<B[j])){
+            merged.push_back(A[i++]);
+        }else{
+            merged.push_back(B[j++]);
         }
     }
 
-    // --- 최종 판정 및 전략 탐색 ---
-    if (checkWinState(isBobWin[1], 0)) {
-        // 1턴, 합 0이 Bob의 필승 상태이므로 Bob이 이김
-        std::cout << "Bob" << std::endl;
-    } else {
-        // Alice가 이기는 경우, 전략을 출력
-        std::cout << "Alice" << std::endl;
-        
-        long long currentSum = 0;
-        // --- 2단계: 앨리스의 최적 경로 탐색 ---
-        for (int i = 1; i <= N; ++i) {
-            const auto& nextLayer = isBobWin[i + 1];
-            const auto& currentTurnCards = turns[i - 1];
+    if(merged.empty()){
+        return {};
+    }
+    vector<pair<long long, long long>> result;
+    result.push_back(merged[0]);
 
-            // 시나리오 1: Alice가 A를 금지할 경우
-            // Bob에게 남는 B, C가 모두 Bob의 패배로 이어지는지 확인
-            bool banA_is_safe = !checkWinState(nextLayer, currentSum + currentTurnCards.cardB) &&
-                                !checkWinState(nextLayer, currentSum + currentTurnCards.cardC);
-            if (banA_is_safe) {
-                std::cout << "Ban " << currentTurnCards.cardA << std::endl;
-                // Bob은 남은 카드 중 작은 것을 선택한다고 가정하여 합계 업데이트
-                currentSum += currentTurnCards.cardB;
-                continue;
-            }
+    for(size_t k= 1; k<merged.size(); ++k){
+        if(merged[k].first<=result.back().second+1){
+            result.back().second= max(result.back().second, merged[k].second);
+        }
+        else{
+            result.push_back(merged[k]);
+        }
+    }
+    return result;
+}
 
-            // 시나리오 2: Alice가 B를 금지할 경우
-            bool banB_is_safe = !checkWinState(nextLayer, currentSum + currentTurnCards.cardA) &&
-                                !checkWinState(nextLayer, currentSum + currentTurnCards.cardC);
-            if (banB_is_safe) {
-                std::cout << "Ban " << currentTurnCards.cardB << std::endl;
-                currentSum += currentTurnCards.cardA;
-                continue;
-            }
 
-            // 시나리오 3: Alice가 C를 금지할 경우
-            // (Alice가 이기는 게임에서는 위 세 시나리오 중 하나는 반드시 참임)
-            std::cout << "Ban " << currentTurnCards.cardC << std::endl;
-            currentSum += currentTurnCards.cardA;
+bool need_debug() {
+
+    // 이 함수는 딱 한번 호출됩니다.
+    // 실제 게임이 어떻게 되었는지 확인하고 싶으면 true를 return하면 됩니다. 이외에는 false를 return하면 됩니다.
+    // true를 return해도 채점이 되긴 하지만, 정답을 최종 제출할 땐 false를 return하는 상태로 제출하는것을 권장합니다.
+
+    // return false; // 정답을 제출할 때에는 false로 바꿔주세요.
+    return false;
+    
+}
+
+void alice_init(long long n, long long l, long long r, const std::vector<std::vector<long long>>& v) {
+    
+    N = n, L = l, R = r;
+    BobWinIntervals.assign(N + 1, vector<pair<long long, long long>>());
+    BobWinIntervals[N].push_back({L,R}); 
+    //N턴 후에 [L,R] 구간에 Bob 있으면 승리
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 3; j++) arr[i][j] = v[i][j];
+    }
+
+    for(int i=N-1; i>=0; --i){
+        long long cardA = v[i][0];
+        long long cardB = v[i][1];
+        long long cardC= v[i][2];
+
+        const auto& next_intervals = BobWinIntervals[i+1];
+
+        //i +! 턴의 승리 구간들로부터 i턴의 승리 후보 구간들 계산
+        vector<pair <long long, long long>> i_A, i_B, i_C;
+        for(const auto&p : next_intervals){
+            i_A.push_back({p.first-cardA, p.second - cardA});
+            i_B.push_back({p.first-cardB, p.second - cardB});
+            i_C.push_back({p.first-cardC, p.second - cardC});
+        }
+
+        // 3개의 후보 중 2 개 이상 포함되는 구간 찾기
+        auto AB=intersect(i_A, i_B);
+        auto AC= intersect(i_A, i_C);
+        auto BC= intersect(i_B, i_C);
+
+        auto AB_AC= union_i(AB, AC);
+        BobWinIntervals[i]= union_i(AB_AC, BC);
+    }
+
+    // 이 함수는 딱 한번 호출됩니다. 
+    // 여기서 입력을 보관하고, 필요한 무거운 연산들인 전처리/사전 계산 등을 모두 끝내야 합니다.
+    // 이후 alice_ban()를 반복적으로 실행할 때, 충분히 빠르게 동작해야 합니다.
+    // 총 수행시간 중 채점기와 Bob의 코드의 총 수행시간은 5초 미만임을 보장합니다.
+    // 즉 alice_init, is_alice_win을 한번 실행하고 alice_ban을 N번 실행하는 시간의 총 합은 15초 미만이어야 합니다.
+
+}
+
+bool is_Bob_win(long long turn, long long S) {
+
+    // 이 함수는 alice_init 이후에 딱 한번 호출됩니다. 
+    // 앨리스가 승리할 수 있으면 true, 아니면 false를 return해야 합니다.
+    // 정답과 다른 값을 return하면 오답으로 판정됩니다.
+    const auto& intervals = BobWinIntervals[turn];
+    if(intervals.empty()) return false;
+
+    auto it =lower_bound(intervals.begin(), intervals.end(), make_pair(S,S));
+
+    if(it!= intervals.end() && it->first <= S && S<= it->second){
+        return true;
+    }
+
+    if(it!=intervals.begin()){
+        --it;
+        if(it->first<=S && S<= it->second){
+            return true;
         }
     }
 
-    return 0;
+    return false;
+
+}
+
+bool is_alice_win(){
+    return !is_Bob_win(0,0);
+}
+
+int alice_ban(int turn, long long S) { //turn은 0-base로 들어옵니다.
+
+    // is_alice_win()에서 true를 return하면 호출되는 함수이며, 정확히 N번 호출되는 함수입니다.
+    // 매 호출시 현재 진행중인 턴수와 S값이 주어집니다. 이번 턴에 앨리스가 밴할 인덱스를 return해야 합니다.
+    // main함수에서는 alice_ban과 bob_pick함수가 서로 번갈아가면서 실행됩니다. 
+    // turn은 0-base로 들어옵니다. 즉 turn = [0..N-1]이 순차적으로 들어옵니다.
+    // 즉 현재 밴한 결과는 다음 bob_pick 함수 호출때 영향을 미치며 다음 alice_ban 함수에도 연쇄적으로 영향을 끼칩니다.
+    // indx는 0-base입니다. 즉 [0..2]사이의 int를 return해야합니다.
+    // 그 외의 값을 return할 시 채점 결과는 오답으로 판정되고 프로그램을 즉시 종료합니다.
+    // 최종 S값이 [L..R]사이에 들어오면 오답으로 판정됩니다, 들어오지 않는다면 정답으로 판정됩니다.
+
+    long long A = arr[turn][0];
+    long long B= arr[turn][1];
+    long long C= arr[turn][2];
+
+    if(!is_Bob_win(turn+1, S+B) && !is_Bob_win(turn+1, S+C)){
+        return 0;
+    }
+    if(!is_Bob_win(turn+1, S+A) && !is_Bob_win(turn+1, S+C)){
+        return 1;
+    }
+    return 2;
+
 }
